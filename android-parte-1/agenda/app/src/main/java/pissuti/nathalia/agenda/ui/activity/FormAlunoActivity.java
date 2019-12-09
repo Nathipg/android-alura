978bc3374bfb82c84aaecb3aefb8c77b7a218540
Nathália Pissuti <nathaliapissuti@gmail.com>
2019-12-09 16:26:13 -0300

    android parte 1 Aula 4

---
+++ android-parte-1/agenda/app/src/main/java/pissuti/nathalia/agenda/ui/activity/FormAlunoActivity.java
@@ -0,0 +1,16 @@
+package pissuti.nathalia.agenda.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pissuti.nathalia.agenda.R;
+
+public class FormAlunoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_aluno);
+    }
+}
