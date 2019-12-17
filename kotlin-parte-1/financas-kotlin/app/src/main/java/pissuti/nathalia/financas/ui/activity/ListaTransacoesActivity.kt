27de397e6e1af932fd95c96ddd71d33f61e78f44
Nathália Pissuti <nathaliapissuti@gmail.com>
2019-12-17 09:53:01 -0300

    kotlin parte 1 aula 3

--- kotlin-parte-1/financas-kotlin/app/src/main/java/pissuti/nathalia/financas/ui/activity/ListaTransacoesActivity.kt
+++ kotlin-parte-1/financas-kotlin/app/src/main/java/pissuti/nathalia/financas/ui/activity/ListaTransacoesActivity.kt
@@ -1,4 +1,23 @@
 package pissuti.nathalia.financas.ui.activity
 
-class ListaTransacoesActivity {
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_lista_transacoes.*
+import pissuti.nathalia.financas.R
+import pissuti.nathalia.financas.ui.adapter.ListaTransacoesAdapter
+import kotlin.collections.listOf
+
+class ListaTransacoesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) : Unit {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_transacoes)
+
+        val transacoes: List<String> = listOf("Comida - R$ 20", "Economia - R$ 100")
+
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, transacoes)
+
+        lista_transacoes_listview.setAdapter(ListaTransacoesAdapter(transacoes))
+    }
 }
--- kotlin-parte-1/financas-kotlin/app/src/main/java/pissuti/nathalia/financas/ui/adapter/ListaTransacoesAdapter.kt
+++ kotlin-parte-1/financas-kotlin/app/src/main/java/pissuti/nathalia/financas/ui/adapter/ListaTransacoesAdapter.kt
@@ -1,4 +1,28 @@
 package pissuti.nathalia.financas.ui.adapter
 
-class ListaTransacoesAdapter {
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+
+class ListaTransacoesAdapter(transacoes: List<String>) : BaseAdapter() {
+
+    private val transacoes = transacoes;
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItem(position: Int): String {
+        return transacoes[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getCount(): Int {
+        return transacoes.size
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 }
