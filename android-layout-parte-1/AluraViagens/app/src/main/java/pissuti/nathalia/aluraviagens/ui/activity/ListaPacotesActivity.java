e3a06d7ff869faabced297cf32054ec9b4e5d9a1
Nathália Pissuti <nathaliapissuti@gmail.com>
2020-03-02 14:52:52 -0300

    android layout parte 1 Aula 5

--- android-layout-parte-1/AluraViagens/app/src/main/java/pissuti/nathalia/aluraviagens/ui/activity/ListaPacotesActivity.java
+++ android-layout-parte-1/AluraViagens/app/src/main/java/pissuti/nathalia/aluraviagens/ui/activity/ListaPacotesActivity.java
@@ -3,8 +3,14 @@ package pissuti.nathalia.aluraviagens.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.List;
 
 import pissuti.nathalia.aluraviagens.R;
+import pissuti.nathalia.aluraviagens.dao.PacoteDAO;
+import pissuti.nathalia.aluraviagens.model.Pacote;
+import pissuti.nathalia.aluraviagens.ui.adapter.ListaPacotesAdapter;
 
 public class ListaPacotesActivity extends AppCompatActivity {
 
@@ -12,5 +18,10 @@ public class ListaPacotesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_pacotes);
+        ListView listaDePacotes = findViewById(R.id.lista_pacotes_listview);
+
+        List<Pacote> pacotes = new PacoteDAO().lista();
+
+        listaDePacotes.setAdapter(new ListaPacotesAdapter(pacotes, this));
     }
 }
---
+++ android-layout-parte-1/AluraViagens/app/src/main/java/pissuti/nathalia/aluraviagens/ui/adapter/ListaPacotesAdapter.java
@@ -0,0 +1,74 @@
+package pissuti.nathalia.aluraviagens.ui.adapter;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+import pissuti.nathalia.aluraviagens.R;
+import pissuti.nathalia.aluraviagens.model.Pacote;
+
+public class ListaPacotesAdapter extends BaseAdapter {
+
+    private final List<Pacote> pacotes;
+    private Context context;
+
+    public ListaPacotesAdapter(List<Pacote> pacotes, Context context) {
+        this.pacotes = pacotes;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return pacotes.size();
+    }
+
+    @Override
+    public Pacote getItem(int position) {
+        return pacotes.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View viewCriada = LayoutInflater
+                .from(context)
+                .inflate(R.layout.item_pacote, parent, false);
+
+        Pacote pacote = pacotes.get(position);
+
+        TextView local = viewCriada.findViewById(R.id.item_pacote_local);
+        local.setText(pacote.getLocal());
+
+        // Pegar Imagem
+        Resources resources = context.getResources();
+        int idDoDrawable = resources.getIdentifier(pacote.getImagem(), "drawable", context.getPackageName());
+        Drawable drawableImagemPacote = resources.getDrawable(idDoDrawable);
+
+        ImageView imagem = viewCriada.findViewById(R.id.item_pacote_imagem);
+        imagem.setImageDrawable(drawableImagemPacote);
+
+        TextView dias = viewCriada.findViewById(R.id.item_pacote_dias);
+        dias.setText(pacote.getDias() + " dias");
+
+        TextView preco = viewCriada.findViewById(R.id.item_pacote_preco);
+        preco.setText("R$ " + pacote.getPreco().toString());
+
+        return viewCriada;
+    }
+
+}
