1ee76d16d07888aaa48481d54e4f94b47cfd814c
Nathália Pissuti <nathaliapissuti@gmail.com>
2020-03-03 16:44:50 -0300

    android layout parte 2 Aula 3

--- android-layout-parte-2/AluraViagens/app/src/main/java/pissuti/nathalia/aluraviagens/ui/activity/ResumoPacoteActivity.java
+++ android-layout-parte-2/AluraViagens/app/src/main/java/pissuti/nathalia/aluraviagens/ui/activity/ResumoPacoteActivity.java
@@ -1,5 +1,6 @@
 package pissuti.nathalia.aluraviagens.ui.activity;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -36,6 +37,9 @@ public class ResumoPacoteActivity extends AppCompatActivity {
         mostraDias(pacoteSaoPaulo);
         mostraPreco(pacoteSaoPaulo);
         mostraData(pacoteSaoPaulo);
+
+        Intent intent = new Intent(this, PagamentoActivity.class);
+        startActivity(intent);
     }
 
     private void mostraData(Pacote pacote) {
