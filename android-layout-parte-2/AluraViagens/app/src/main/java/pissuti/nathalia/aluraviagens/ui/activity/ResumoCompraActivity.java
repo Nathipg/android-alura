d49abb548591a81ce06ef0df0d7fb35adefde83e
Nathália Pissuti <nathaliapissuti@gmail.com>
2020-03-04 15:28:31 -0300

    android layout parte 2 Aula 4

---
+++ android-layout-parte-2/AluraViagens/app/src/main/java/pissuti/nathalia/aluraviagens/ui/activity/ResumoCompraActivity.java
@@ -0,0 +1,59 @@
+package pissuti.nathalia.aluraviagens.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
+
+import pissuti.nathalia.aluraviagens.R;
+import pissuti.nathalia.aluraviagens.model.Pacote;
+import pissuti.nathalia.aluraviagens.util.DataUtil;
+import pissuti.nathalia.aluraviagens.util.MoedaUtil;
+import pissuti.nathalia.aluraviagens.util.ResourcesUtil;
+
+public class ResumoCompraActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Resumo da compra";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resumo_compra);
+
+        setTitle(TITULO_APPBAR);
+
+        Pacote pacoteSaoPaulo = new Pacote("São Paulo", "sao_paulo_sp", 2, new BigDecimal("243.99"));
+
+        mostraLocal(pacoteSaoPaulo);
+        mostraImagem(pacoteSaoPaulo);
+        mostraData(pacoteSaoPaulo);
+        mostraPreco(pacoteSaoPaulo);
+    }
+
+    private void mostraPreco(Pacote pacote) {
+        TextView preco = findViewById(R.id.resumo_compra_preco_pacote);
+        String moedaBrasileira = MoedaUtil.formataParaBr(pacote.getPreco());
+        preco.setText(moedaBrasileira);
+    }
+
+    private void mostraData(Pacote pacote) {
+        TextView data = findViewById(R.id.resumo_compra_data_viagem);
+        String periodoEmTexto = DataUtil.periodoEmTexto(pacote.getDias());
+        data.setText(periodoEmTexto);
+    }
+
+    private void mostraImagem(Pacote pacote) {
+        ImageView imagem = findViewById(R.id.resumo_compra_imagem_pacote);
+        Drawable drawablePacote = ResourcesUtil.getDrawable(this, pacote.getImagem());
+        imagem.setImageDrawable(drawablePacote);
+    }
+
+    private void mostraLocal(Pacote pacote) {
+        TextView local = findViewById(R.id.resumo_compra_local_pacote);
+        local.setText(pacote.getLocal());
+    }
+}
