a52b5d1cc36cdec6a0379534884b6df7b2d04a58
Nathália Pissuti <nathaliapissuti@gmail.com>
2020-03-04 10:23:44 -0300

    android layout parte 2 Aula 3

--- android-layout-parte-2/AluraViagens/app/src/main/java/pissuti/nathalia/aluraviagens/ui/activity/PagamentoActivity.java
+++ android-layout-parte-2/AluraViagens/app/src/main/java/pissuti/nathalia/aluraviagens/ui/activity/PagamentoActivity.java
@@ -3,14 +3,33 @@ package pissuti.nathalia.aluraviagens.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
 
 import pissuti.nathalia.aluraviagens.R;
+import pissuti.nathalia.aluraviagens.model.Pacote;
+import pissuti.nathalia.aluraviagens.util.MoedaUtil;
 
 public class PagamentoActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Pagamento";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagamento);
+
+        setTitle(TITULO_APPBAR);
+
+        Pacote pacoteSaoPaulo = new Pacote("São Paulo", "sao_paulo_sp", 2, new BigDecimal("243.99"));
+
+        mostraPreco(pacoteSaoPaulo);
+    }
+
+    private void mostraPreco(Pacote pacote) {
+        TextView preco = findViewById(R.id.pagamento_preco_pacote);
+        String moedaBrasileira = MoedaUtil.formataParaBr(pacote.getPreco());
+        preco.setText(moedaBrasileira);
     }
 }
